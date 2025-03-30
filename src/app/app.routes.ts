e4f76d9d2d153be26cc25427c5cb3e371fd233d6
Your Name <you@example.com>
2025-03-30 13:51:32 -0500

    Adding starter files for practice-Routing

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+
+export const routes: Routes = [
+
+];
+
+
